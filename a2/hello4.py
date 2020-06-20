4afd540c604a37353cc4cceb6aa522e20c3668c3
Victor Cheng <59785657+victorcheng0425@users.noreply.github.com>
2020-06-20 11:13:18 -0700

    Add files via upload

---
+++ a2/hello4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Note: This is going to be *much* more portable -- it finds the
+# location of the "python3" executable, and runs the rest of the
+# script with the command. This means the location of "python3" does
+# *not* need to be hard-coded into the bang path.
+
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
